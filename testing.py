67ccd9ebe72cf5b26c7d0d407dce788ba83c7157
gaucho212 <adamwoz2005@gmail.com>
2025-03-13 17:40:53 +0100

    Added testing for all lists

--- testing.py
+++ testing.py
@@ -1,8 +1,5 @@
 import time
-import random
-import sorting  
-
-
+import sorting
 
 # List of sorting algorithms to test
 sorting_algorithms = [
@@ -11,11 +8,20 @@ sorting_algorithms = [
     sorting.quick_sort_random_pivot,
     sorting.heap_sort,
     sorting.insertion_sort,
-    sorting.selection_sort
+    sorting.selection_sort,
 ]
 
 # List of sizes for the random lists
 list_sizes = [2**x for x in range(2, 20)]
+
+# List of categories for different list shapes
+categories = [
+    "a_shaped_array",
+    "constant_array",
+    "decreasing_array",
+    "increasing_array",
+    "random_array",
+]
 
 # Dictionary to store the results
 benchmark_results = {}
@@ -24,19 +30,25 @@ benchmark_results = {}
 for algorithm in sorting_algorithms:
     algorithm_name = algorithm.__name__
     benchmark_results[algorithm_name] = {}
-    for size in list_sizes:
-        f=open(f'benchmark/a_shaped_array_{size:08d}.txt','r')
-        testing_list = [int(x) for x in f.read().split()]
-        start_time = time.time()
-        algorithm(testing_list.copy())  # Use a copy to avoid in-place sorting issues
-        end_time = time.time()
-        duration = end_time - start_time
-        benchmark_results[algorithm_name][size] = duration
+    for category in categories:
+        benchmark_results[algorithm_name][category] = {}
+        for size in list_sizes:
+            f = open(f"benchmark/{category}_{size:08d}.txt", "r")
+            testing_list = [int(x) for x in f.read().split()]
+            start_time = time.time()
+            algorithm(
+                testing_list.copy()
+            )  # Use a copy to avoid in-place sorting issues
+            end_time = time.time()
+            duration = end_time - start_time
+            benchmark_results[algorithm_name][category][size] = duration
 
 # Save the results to a file
-with open('benchmark_results.txt', 'w') as file:
-    for algorithm_name, results in benchmark_results.items():
+with open("benchmark_results.txt", "w") as file:
+    for algorithm_name, categories in benchmark_results.items():
         file.write(f"{algorithm_name}:\n")
-        for size, duration in results.items():
-            file.write(f"  List size {size}: {duration:.6f} seconds\n")
-        file.write("\n")
+        for category, results in categories.items():
+            file.write(f"  {category}:\n")
+            for size, duration in results.items():
+                file.write(f"    List size {size}: {duration:.6f} seconds\n")
+            file.write("\n")
